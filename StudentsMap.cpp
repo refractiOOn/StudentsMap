c3208e85c58ae8386bb41c4e6385871daf7b7f93
refractioN <etozhesotnikow@gmail.com>
2021-11-14 22:12:44 +0200

    done

--- StudentsMap.cpp
+++ StudentsMap.cpp
@@ -22,15 +22,25 @@ int main()
 	StudentInfo roman("Roman", "Sotnikow", 100);
 	StudentInfo alexey("Alexey", "Myheenko", 80);
 	StudentInfo michael("Michael", "Litvinovich", 90);
+	StudentInfo daniel("Daniel", "Yan", 85);
 	students.emplace(1, roman);
 	students.emplace(2, alexey);
 	students.emplace(3, michael);
+	students.emplace(4, daniel);
 	for (auto it = students.cbegin(); it != students.cend(); std::advance(it, 1))
 	{
 		std::cout << it->first << " " << it->second.m_firstName << " " << it->second.m_lastName << " " << it->second.m_averageScore << std::endl;
 	}
-	auto result = find_if(students.cbegin(), students.cend(), [](const StudentInfo& info)
+	auto result = find_if(students.cbegin(), students.cend(), [](const std::pair<const int, StudentInfo>& info)
 		{
-
+			return info.second.m_averageScore == 100;
 		});
+	if (result == students.end())
+	{
+		std::cout << "Student not found" << std::endl;
+	}
+	else
+	{
+		std::cout << "Student's name is " << result->second.m_firstName << " " << result->second.m_lastName << std::endl;
+	}
 }
